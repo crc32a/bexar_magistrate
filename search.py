b1ef19ccf67793127242d37b2bce67029245d247
Carlos D. Garza <carlos.garza@rackspace.com>
2018-05-24 17:17:10 -0500

    adding arrest date sorting to search.py

--- search.py
+++ search.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import scrape_magistrate as sm
+import datetime
 import sys
 import re
 
+months = {'January':1, 'February':2, 'March':3, 
+           'April':4, 'May':5, 'June':6, 
+           'July':7,  'August':8, 'September':9, 
+           'October':10, 'November':11, 'December':12}
 
+date_restr="(\S*)\s([0-9]{,2}),([0-9]{,4}) at ([0-9]{,2}):([0-9]{,2})"
+date_re = re.compile(date_restr, re.IGNORECASE)
+
+def getdate(date_str):
+    default_dt = datetime.datetime(1970,1,1,0,0)
+    m = date_re.match(date_str)
+    if not m:
+        return default_dr
+    year = int(m.group(3))
+    if m.group(1) not in months:
+        return default_dt
+    month = months[m.group(1)]
+    day = int(m.group(2))
+    hour = int(m.group(4))
+    min = int(m.group(5))
+    dt = datetime.datetime(year, month, day, hour)
+    return dt
 
 def main(*args):
     if len(args)<3:
@@ -20,7 +42,15 @@ def main(*args):
     charge_re = re.compile(".*" + charge_str + ".*", re.IGNORECASE)
     all = sm.load_json("all.json")
 
-    for (intake_num, inmate) in all.items():
+    arrest_order = []
+
+    for(intake_num, inmate) in all.items():
+        arrest = inmate['Arrest Date/Time']
+        dt = getdate(arrest)
+        arrest_order.append( (dt, intake_num, inmate))
+    arrest_order.sort()
+
+    for (dt, intake_num, inmate) in arrest_order:
         name = inmate['Name']
         age = sm.inmate_age(inmate)
         sex = inmate['Sex']
